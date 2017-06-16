22e0df402bab423467f4ff5287dc8638efae5adb
Sachin Jain <sachinjain.0101@gmail.com>
2017-06-16 01:06:05 -0400

    Email with HTML format

--- PyMail/PyMail/PyMail.py
+++ PyMail/PyMail/PyMail.py
@@ -6,19 +6,34 @@ import sys
 
 message = '\r\n'.join(['From: {0}',
           'To: {1}',
-          'Subject: Just another message',
+          'MIME-Version: 1.0',
+          'Content-type: text/html',
+          'Subject: Second HTML message',
           '',
-          'Why, oh why'])
+          '{2}'])
+
+body="""
+        <!DOCTYPE html>
+        <html>
+        <body>
+
+        <h1>My First Heading</h1>
+
+        <p>My first paragraph.</p>
+
+        </body>
+        </html>
+     """
 
 try:
     server = smtplib.SMTP(Vars.SMTP_ADDR)
     server.ehlo()
     server.starttls()
     server.login(Vars.SMTP_USER, Vars.SMTP_PWD)
-    server.sendmail(Vars.EMAIL_FROM, Vars.EMAIL_TO, message.format(Vars.EMAIL_FROM,Vars.EMAIL_TO))
+    server.sendmail(Vars.EMAIL_FROM, Vars.EMAIL_TO, message.format(Vars.EMAIL_FROM,Vars.EMAIL_TO,body))
     server.close()
     print 'successfully sent the mail'
 except:
     print 'failed to send mail'
 
-sys.exit(0)
+#sys.exit(0)
